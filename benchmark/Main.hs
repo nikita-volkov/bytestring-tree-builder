a3ff0fed8498a2f9e9ebf10e996489b8f3673b08
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-05-27 11:48:21 +0300

    Drop the notorious ImpredicativeTypes extension

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -11,37 +11,43 @@ main =
   defaultMain $
   map sampleGroup $
   [("Small Input", smallSample), ("Medium Input", mediumSample), ("Large Input", largeSample)]
-    
+
 sampleGroup :: (String, Sample) -> Benchmark
-sampleGroup (title, sample) =
+sampleGroup (title, (Sample sample)) =
   bench title $ nf sample $
   (A.byteString, mappend, mempty, A.toByteString)
 
-type Sample =
-  forall a. (ByteString -> a, a -> a -> a, a, a -> ByteString) -> ByteString
+newtype Sample =
+  Sample (forall a. (ByteString -> a, a -> a -> a, a, a -> ByteString) -> ByteString)
 
 {-# NOINLINE smallSample #-}
 smallSample :: Sample
-smallSample (fromByteString, (<>), mempty, toByteString) =
-  toByteString $
+smallSample =
+  Sample $
+  \ (fromByteString, (<>), mempty, toByteString) ->
+    toByteString $
     (fromByteString "hello" <> fromByteString "asdf") <>
     fromByteString "fsndfn" <>
     (fromByteString "dfgknfg" <> fromByteString "aaaaaa")
 
 {-# NOINLINE mediumSample #-}
 mediumSample :: Sample
-mediumSample (fromByteString, (<>), mempty, toByteString) =
-  toByteString $
-  foldl' (<>) mempty $ replicate 1000 $
+mediumSample =
+  Sample $
+  \ (fromByteString, (<>), mempty, toByteString) ->
+    toByteString $
+    foldl' (<>) mempty $ replicate 1000 $
     (fromByteString "hello" <> fromByteString "asdf") <>
     fromByteString "fsndfn" <>
     (fromByteString "dfgknfg" <> fromByteString "aaaaaa")
 
 {-# NOINLINE largeSample #-}
 largeSample :: Sample
-largeSample (fromByteString, (<>), mempty, toByteString) =
-  toByteString $
-  foldl' (<>) mempty $ replicate 100000 $
+largeSample =
+  Sample $
+  \ (fromByteString, (<>), mempty, toByteString) ->
+    toByteString $
+    foldl' (<>) mempty $ replicate 100000 $
     (fromByteString "hello" <> fromByteString "asdf") <>
     fromByteString "fsndfn" <>
     (fromByteString "dfgknfg" <> fromByteString "aaaaaa")
