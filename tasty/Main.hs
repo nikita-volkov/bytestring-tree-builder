9d7fc762f09007d816a3eef807a68ebc88f6b9c2
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-11-28 11:40:03 +0300

    Reproduce regression

--- tasty/Main.hs
+++ tasty/Main.hs
@@ -24,4 +24,16 @@ tree =
     \bytestrings ->
       mconcat bytestrings ===
       Data.ByteString.Lazy.toStrict (toLazyByteString (foldMap byteString bytestrings))
+    ,
+    testCase "Regression (Issue #5)" $
+    let
+      fn name =
+        toByteString $
+          "FETCH FORWARD "
+            <> asciiIntegral 10
+            <> " FROM "
+            <> byteString name
+      in assertEqual ""
+        "FETCH FORWARD 10 FROM abc"
+        (fn "abc")
   ]
